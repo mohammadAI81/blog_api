2d2b6404c112e44e478c1ebc1f513ea7a46965b9
mohammad ghorashi <mohammadghorashi81@gmail.com>
2024-03-27 00:39:38 +0330

    change APIview to ModelViewSet

--- blog/urls.py
+++ blog/urls.py
@@ -3,9 +3,12 @@ from rest_framework import routers
 
 from . import views
 
+router = routers.DefaultRouter()
+router.register('blogs', views.Blogs, basename='blog')
 
+urlpatterns = router.urls
 
-urlpatterns = [
-    path('', views.Blogs.as_view(), name='blogs'),
-    path('<int:pk>/', views.DetailBlog.as_view(), name='edit_blog'),
-]
+# urlpatterns = [
+#     path('', views.Blogs.as_view(), name='blogs'),
+#     path('<int:pk>/', views.DetailBlog.as_view(), name='edit_blog'),
+# ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,3 @@
-from django.shortcuts import get_object_or_404
-
-from rest_framework.decorators import APIView, api_view
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from rest_framework import status
@@ -9,41 +6,40 @@ from .models import Blog, Comment
 from .serializer import BlogSerializer, CommetSerializer
 
 
-# class Blogs(ModelViewSet):
-#     queryset = Blog.objects.filter(published='pub')
-#     serializer_class = BlogSerializer
-
-
-class Blogs(APIView):
-    def get(self, request):
-        blogs = Blog.objects.filter(published = 'pub')
-        serializer = BlogSerializer(blogs, many=True)
-        return Response(serializer.data)
+class Blogs(ModelViewSet):
+    queryset = Blog.objects.filter(published='pub')
+    serializer_class = BlogSerializer
     
-    def post(self, request):
-        serializer = BlogSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        print(serializer.validated_data)
-        serializer.save()
-        return Response(serializer.data)
+
+# class Blogs(APIView):
+#     def get(self, request):
+#         blogs = Blog.objects.filter(published = 'pub')
+#         serializer = BlogSerializer(blogs, many=True)
+#         return Response(serializer.data)
+    
+#     def post(self, request):
+#         serializer = BlogSerializer(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         print(serializer.validated_data)
+#         serializer.save()
+#         return Response(serializer.data)
 
 
-class DetailBlog(APIView):
-    def get(self, request, pk):
-        blog = get_object_or_404(Blog, pk=pk)
-        comments = Comment.objects.filter(post=blog)
-        serializer = BlogSerializer(blog)
-        serializer_ = CommetSerializer(comments, many= True)
-        return Response(serializer.data)
+# class DetailBlog(APIView):
+#     def get(self, request, pk):
+#         blog = get_object_or_404(Blog, pk=pk)
+#         comments = Comment.objects.filter(post=blog)
+#         serializer = BlogSerializer(blog)
+#         serializer_ = CommetSerializer(comments, many= True)
+#         return Response(serializer.data)
     
-    def delete(self, request, pk):
-        blog = get_object_or_404(Blog, pk=pk)
-        blog.delete()
-        return Response({'Delete': 'Not Defind'}, status=status.HTTP_204_NO_CONTENT)
+#     def delete(self, request, pk):
+#         blog = get_object_or_404(Blog, pk=pk)
+#         blog.delete()
+#         return Response({'Delete': 'Not Defind'}, status=status.HTTP_204_NO_CONTENT)
     
-    def patch(self, request):
-        serializer = BlogSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        print(serializer.validated_data)
-        serializer.save()
-        return Response(serializer.data)
+#     def patch(self, request):
+#         serializer = BlogSerializer(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(serializer.data)
